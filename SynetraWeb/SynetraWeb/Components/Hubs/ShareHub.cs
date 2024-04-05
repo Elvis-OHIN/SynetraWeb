d2f2ce650a18a4ea8b9c00ab663660c6dec35162
elvis <elvisohin33@gmail.com>
2024-04-05 22:22:57 +0200

    footPrint

--- SynetraWeb/SynetraWeb/Components/Hubs/ShareHub.cs
+++ SynetraWeb/SynetraWeb/Components/Hubs/ShareHub.cs
@@ -4,14 +4,51 @@ using Microsoft.AspNetCore.SignalR.Client;
 using SynetraUtils.Models.DataManagement;
 using SynetraUtils.Models.MessageManagement;
 using SynetraWeb.Client.Models;
+using SynetraWeb.Client.Services;
+using SynetraWeb.Components.SignalR;
 
 namespace SynetraWeb.Components.Hubs
 {
     public class ShareHub : Hub
     {
+        private readonly static ConnectionMapping<string> _connections = 
+            new ConnectionMapping<string>();
+        /*public override async Task<Task> OnConnectedAsync()
+        {
+            var name = Context.User.Identity.Name;
+            
+            _connections.Add(name, Context.ConnectionId);
+            var param1 = Context.GetHttpContext().Request.Query["param1"];
+            string key = "clé de cryptage partagée";
+            
+            /*ComputerService computers = new ComputerService();
+            var computer =  computers.GetByIdAsync(1);
+        
+            string decryptedId = Auth.Decrypt(param1, key);
+            if (decryptedId == "")
+            {
+                 await SendDataOfPc(Context.ConnectionId);
+            }
+            
+            // Utilisez decryptedId pour l'authentification ou toute autre logique
+            return base.OnConnectedAsync();
+        }*/
+        
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
         public Task SendImageMessage(ImageMessage file)
         {
             return Clients.All.SendAsync("ImageMessage", file);
+        }
+        public async Task Send(string userId, double x, double y ,  double height , double width)
+        {
+            await Clients.User(userId).SendAsync("ReceiveMouseMovement", x, y , height , width);
+        }
+        public async Task SendDataOfPc(string userId)
+        {
+            await Clients.User(userId).SendAsync("ReceiveDataOfPc");
         }
         public async Task SendMessage(string title, string message)
         {
